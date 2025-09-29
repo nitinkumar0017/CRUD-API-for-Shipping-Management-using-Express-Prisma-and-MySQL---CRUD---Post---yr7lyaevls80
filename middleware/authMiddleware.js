1426b94706a57d3c0543398634953de279f81ac6
Nitin-0017 <raonitinkumar06@gmail.com>
2025-09-29 17:54:03 +0530

    done

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,24 +1,18 @@
 const verifySecret = (req, res, next) => {
-  try {
-    console.log(req.headers);
-    const incomingShippingSecret = req.headers["shipping_secret_key"];
-    const localSecretKey = process.env.SHIPPING_SECRET_KEY;
+  const incomingShippingSecret = req.headers["shipping_secret_key"];
+  const localSecretKey = process.env.SHIPPING_SECRET_KEY;
 
-    if (!incomingShippingSecret) {
-      return res.status(403).json({
-        error: "SHIPPING_SECRET_KEY is missing or invalid",
-      });
-    }
-
-    if (incomingShippingSecret !== localSecretKey) {
-      return res.status(403).json({
-        error: "Failed to authenticate SHIPPING_SECRET_KEY",
-      });
-    }
-    next();
-  } catch (error) {
-    console.log(error.message);
-    res.status(403).json({error:error})
+  if (!incomingShippingSecret) {
+    return res.status(403).json({
+      error: "SHIPPING_SECRET_KEY is missing or invalid",
+    });
   }
+
+  if (incomingShippingSecret !== localSecretKey) {
+    return res.status(403).json({
+      error: "Failed to authenticate SHIPPING_SECRET_KEY",
+    });
+  }
+  next();
 };
 module.exports = { verifySecret };
